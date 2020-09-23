4ae726561bd908894d97d927fbd3f96ee95cabf3
aatiq7 <aatiq.rahman01@gmail.com>
2020-09-24 00:04:28 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div style={{backgroundColor: '#278595'}}>
+            <h1 style ={{color: 'white', textAlign: 'center', padding: '15px 0'}}>Welcome to Friends Zone</h1>
+        </div>
+    );
+};
+
+export default Header;
